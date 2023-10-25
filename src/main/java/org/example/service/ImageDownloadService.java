7635e87430771cc7551246e18d22b8744270dda4
Farhan <farhan.ronok@elinvar.de>
2023-10-25 17:49:39 +0200

    code refactor with meaningful method name, remove used code line

--- src/main/java/org/example/service/ImageDownloadService.java
+++ src/main/java/org/example/service/ImageDownloadService.java
@@ -13,7 +13,6 @@ import java.util.regex.Pattern;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.example.service.NioImageDownloader.ImageDownload;
@@ -21,9 +20,11 @@ import org.example.service.NioImageDownloader.NioDownloader;
 
 public class ImageDownloadService {
 
+    private static final String imageRegex = ".*\\.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico)$";
+    private static final String urlRegex = "^(https?|ftp)://[A-Za-z0-9.-]+(:[0-9]+)?(/[A-Za-z0-9%.-]+)*$";
     ImageDownload imageDownload = new NioDownloader();
 
-    public void createCommandLine(String[] args){
+    public void createCommandLine(String[] args) {
         Options options = new Options();
         options.addOption("i", "input", true, "Input file containing image URLs");
         options.addOption("o", "output", true, "Output directory for downloaded images");
@@ -37,21 +38,16 @@ public class ImageDownloadService {
             // Retrieve the values of the specified options
             String inputFile = cmd.getOptionValue("i");
             List<String> strings = readURLsFromFile(inputFile);
+
             String outputDir = cmd.getOptionValue("o");
 
-            for (var x:strings)
-                download(x,outputDir);
+            for (var x : strings) {
+                startDownload(x, outputDir);
+            }
 
         } catch (ParseException e) {
-            System.err.println("Error: " + e.getMessage());
-            // Handle the error or display usage information
-            printUsage(options);
+            System.err.println("Error occurred while reading from CLI : " + e.getMessage());
         }
-    }
-
-    private static void printUsage(Options options) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("ImageDownloaderCLI", options);
     }
 
     public static List<String> readURLsFromFile(String filePath) {
@@ -62,47 +58,42 @@ public class ImageDownloadService {
             while ((line = br.readLine()) != null) {
                 String[] urlsInLine = line.split("\\s+");
                 Arrays.stream(urlsInLine).forEach(x -> {
-                    if (isValidURL(x))
+                    if (checkValidUrl(x)) {
                         Collections.addAll(urlList, x);
-                    else {
-                        System.out.println("Invalid URL : "+ x);
+                    } else {
+                        System.out.println("Invalid URL : " + x);
                     }
                 });
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error occurred while reading from file "+e);
         }
 
         return urlList;
     }
 
-    private String generateFileName(String imageUrl) {
-        // Extract the filename from the URL
-
-        String regex = ".*\\.(jpg|jpeg|png|gif|bmp|tiff|webp|svg|ico)$";
+    private static String generateFileName(String imageUrl) {
         String substring = "";
 
         int lastIndexOfSlash = imageUrl.lastIndexOf('/');
-        if (lastIndexOfSlash >= 0)
+        if (lastIndexOfSlash >= 0) {
             substring = imageUrl.substring(lastIndexOfSlash + 1);
+        }
 
-        if(substring.toLowerCase().matches(regex))
+        if (substring.toLowerCase().matches(imageRegex)) {
             return substring;
+        }
         return "image_" + System.currentTimeMillis() + ".jpg";
     }
 
-    // More about NIO -> https://www.baeldung.com/java-download-file#using-nio
-    private void download(String urlStr, String outputDirectory) {
+    private void startDownload(String urlStr, String outputDirectory) {
         String fileName = generateFileName(urlStr);
         String outputPath = outputDirectory + File.separator + fileName;
         System.out.println("Downloaded : " + outputPath);
-        imageDownload.downloadImage(urlStr,outputPath);
+        imageDownload.downloadImage(urlStr, outputPath);
     }
 
-    public static boolean isValidURL(String url) {
-        // Regular expression for a valid URL
-        String urlRegex = "^(https?|ftp)://[A-Za-z0-9.-]+(:[0-9]+)?(/[A-Za-z0-9%.-]+)*$";
-
+    public static boolean checkValidUrl(String url) {
         Pattern pattern = Pattern.compile(urlRegex);
         Matcher matcher = pattern.matcher(url);
 
